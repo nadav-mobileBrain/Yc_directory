2405375fd3e5204d416244a9999439b279fd4767
nadav-mobileBrain <nadavg@mobile-brain.net>
2024-11-03 15:23:52 +0200

    added types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,13 +3,18 @@ import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupCardType = Omit<Startup, "author"> & {
+  author?: Author;
+};
 
 const StartupCard = ({ post }: { post: StartupCardType }) => {
   const {
     _id,
     _createdAt,
     views,
-    author: { _id: authorId, name },
+    author,
     title,
     description,
     category,
@@ -28,17 +33,17 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
-            src={image!}
-            alt={name!}
+            src={author?.image!}
+            alt={author?.name!}
             width={48}
             height={48}
             className="rounded-full"
