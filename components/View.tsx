49513f44c34328c3c5529e9e23fff132caf0afe2
nadav-mobileBrain <nadavg@mobile-brain.net>
2024-11-03 19:01:45 +0200

    client write

--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,8 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -8,6 +10,13 @@ const View = async ({ id }: { id: string }) => {
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
   // increment views
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
   return (
     <div className="view-container">
       <div className="absolute -top-2">
